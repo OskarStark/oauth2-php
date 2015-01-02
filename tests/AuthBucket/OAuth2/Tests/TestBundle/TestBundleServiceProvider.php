b1d848de8259aaa39b2d18daeefcd75528b50f7d
Wong Hoi Sing Edison <hswong3i@gmail.com>
2015-01-02 20:57:57 +0800

    Remove legacy CRUDL demo.

--- tests/AuthBucket/OAuth2/Tests/TestBundle/TestBundleServiceProvider.php
+++ tests/AuthBucket/OAuth2/Tests/TestBundle/TestBundleServiceProvider.php
@@ -11,13 +11,10 @@
 
 namespace AuthBucket\OAuth2\Tests\TestBundle;
 
-use AuthBucket\OAuth2\Tests\TestBundle\Controller\AuthorizeController;
-use AuthBucket\OAuth2\Tests\TestBundle\Controller\ClientController;
 use AuthBucket\OAuth2\Tests\TestBundle\Controller\DefaultController;
 use AuthBucket\OAuth2\Tests\TestBundle\Controller\DemoController;
 use AuthBucket\OAuth2\Tests\TestBundle\Controller\OAuth2Controller;
 use AuthBucket\OAuth2\Tests\TestBundle\Controller\ResourceController;
-use AuthBucket\OAuth2\Tests\TestBundle\Controller\ScopeController;
 use Silex\Application;
 use Silex\ServiceProviderInterface;
 
@@ -30,30 +27,6 @@ class TestBundleServiceProvider implements ServiceProviderInterface
 {
     public function register(Application $app)
     {
-        $app['authbucket_oauth2.tests.authorize_controller'] = $app->share(function () use ($app) {
-            return new AuthorizeController(
-                $app['validator'],
-                $app['serializer'],
-                $app['authbucket_oauth2.model_manager.factory']
-            );
-        });
-
-        $app['authbucket_oauth2.tests.client_controller'] = $app->share(function () use ($app) {
-            return new ClientController(
-                $app['validator'],
-                $app['serializer'],
-                $app['authbucket_oauth2.model_manager.factory']
-            );
-        });
-
-        $app['authbucket_oauth2.tests.scope_controller'] = $app->share(function () use ($app) {
-            return new ScopeController(
-                $app['validator'],
-                $app['serializer'],
-                $app['authbucket_oauth2.model_manager.factory']
-            );
-        });
-
         $app['authbucket_oauth2.tests.default_controller'] = $app->share(function () {
             return new DefaultController();
         });
